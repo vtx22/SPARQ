b4b2b388b7cb0065cd1a47722f3da1797a062d98
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-09-03 04:12:12 +0200

    ui elements for connection window

--- src/ConnectionWindow.cpp
+++ src/ConnectionWindow.cpp
@@ -1,6 +1,6 @@
 #include "ConnectionWindow.hpp"
 
-ConnectionWindow::ConnectionWindow(/* args */)
+ConnectionWindow::ConnectionWindow()
 {
 }
 
@@ -10,31 +10,31 @@ ConnectionWindow::~ConnectionWindow()
 
 void ConnectionWindow::update()
 {
+    // ImGui::SetNextWindowSizeConstraints(ImVec2(1000.0f, 0.0f), ImVec2(FLT_MAX, FLT_MAX));
     if (ImGui::Begin("Connection"))
     {
-        static std::vector<std::string> com_ports;
+
+        if (_com_ports.size() == 0)
+        {
+            _com_ports.push_back("COM-");
+        }
 
         if (ImGui::Button("Refresh Ports"))
         {
-            com_ports = Serial::get_port_names();
+            _com_ports = Serial::get_port_names();
         }
+        ImGui::SameLine();
+        ImGui::PushItemWidth(-FLT_MIN);
 
-        if (com_ports.size() == 0)
+        if (ImGui::BeginCombo("##", _com_ports[_current_id].c_str()))
         {
-            com_ports.push_back("COM-");
-        }
-
-        static int item_current_idx = 0;
-
-        if (ImGui::BeginCombo("COM Port", com_ports[item_current_idx].c_str()))
-        {
-            for (int n = 0; n < com_ports.size(); n++)
+            for (int n = 0; n < (int)_com_ports.size(); n++)
             {
-                const bool is_selected = (item_current_idx == n);
+                bool is_selected = (_current_id == n);
 
-                if (ImGui::Selectable(com_ports[n].c_str(), is_selected))
+                if (ImGui::Selectable(_com_ports.at(n).c_str(), is_selected))
                 {
-                    item_current_idx = n;
+                    _current_id = n;
                 }
 
                 // Set the initial focus when opening the combo (scrolling + keyboard navigation focus)
@@ -45,6 +45,31 @@ void ConnectionWindow::update()
             }
             ImGui::EndCombo();
         }
+        ImGui::PopItemWidth();
+
+        if (true)
+        {
+            ImGui::BeginDisabled();
+        }
+
+        if (ImGui::Button("Open"))
+        {
+        }
+
+        if (true)
+        {
+            ImGui::EndDisabled();
+        }
     }
     ImGui::End();
+}
+
+int ConnectionWindow::get_selected_index()
+{
+    return _current_id;
+}
+
+std::string ConnectionWindow::get_selected_port()
+{
+    return _com_ports.at(_current_id);
 }
--- src/ConnectionWindow.hpp
+++ src/ConnectionWindow.hpp
@@ -16,7 +16,12 @@ public:
 
     void update();
 
+    std::string get_selected_port();
+    int get_selected_index();
+
 private:
+    int _current_id = 0;
+    std::vector<std::string> _com_ports;
 };
 
 #endif // CONNECTION_WINDOW_HPP
