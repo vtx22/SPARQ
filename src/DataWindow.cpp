6b265683c0e8ffaaad3928aee3d519df6393a030
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-09-10 04:31:24 +0200

    correct implementation of custom name

--- src/DataWindow.cpp
+++ src/DataWindow.cpp
@@ -31,8 +31,6 @@ void DataWindow::dataset_entries(std::vector<sparq_dataset_t> &datasets)
 
         for (uint8_t i = 0; i < datasets.size(); i++)
         {
-            static char name[256];
-
             std::string i_str = std::to_string(i);
 
             ImGui::TableNextRow();
@@ -41,10 +39,7 @@ void DataWindow::dataset_entries(std::vector<sparq_dataset_t> &datasets)
             ImGui::Text("%d", datasets[i].id);
             ImGui::TableSetColumnIndex(1);
             ImGui::SetNextItemWidth(100);
-            ImGui::InputTextWithHint(("##DsNameTB" + i_str).c_str(), "Custom Name", name, 256);
-
-            datasets[i].name = std::string(name);
-
+            ImGui::InputTextWithHint(("##DsNameTB" + i_str).c_str(), "Custom Name", datasets[i].name, 64);
             ImGui::TableSetColumnIndex(2);
             ImGui::ColorEdit4(("##DsColor" + i_str).c_str(), (float *)&datasets[i].color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
             ImGui::TableSetColumnIndex(3);
--- src/PlottingWindow.cpp
+++ src/PlottingWindow.cpp
@@ -19,7 +19,7 @@ void PlottingWindow::update(const std::vector<sparq_dataset_t> &datasets)
 
             for (const auto &ds : datasets)
             {
-                std::string name = (ds.name == "") ? std::to_string(ds.id) : ds.name;
+                std::string name = (ds.name[0] == 0) ? std::to_string(ds.id) : std::string(ds.name);
                 ImPlot::PlotLine((name + "###LP" + std::to_string(ds.id)).c_str(), ds.x_values.data(), ds.y_values.data(), ds.x_values.size());
             }
 
