177120fa7bb164cad847af25b99f01838a92ac8c
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-09-17 12:19:26 +0200

    get x and y values method

--- src/PlottingWindow.cpp
+++ src/PlottingWindow.cpp
@@ -30,24 +30,7 @@ void PlottingWindow::update()
                 std::string name = (ds.name[0] == 0) ? std::to_string(ds.id) : std::string(ds.name);
                 ImPlot::SetNextLineStyle(ds.color, 3);
 
-                std::vector<double> *x_values;
-                std::vector<double> *y_values;
-
-                y_values = _data_handler->interpolation ? &ds.y_values_ip : &ds.y_values;
-
-                switch (_data_handler->x_axis_select)
-                {
-                default:
-                case 0:
-                    x_values = _data_handler->interpolation ? &ds.samples_ip : &ds.samples;
-                    break;
-                case 1:
-                    x_values = _data_handler->interpolation ? &ds.relative_times_ip : &ds.relative_times;
-                    break;
-                case 2:
-                    x_values = _data_handler->interpolation ? &ds.absolute_times_ip : &ds.absolute_times;
-                    break;
-                }
+                auto [x_values, y_values] = get_xy_values(ds);
 
                 ImPlot::PlotLine((name + "###LP" + std::to_string(ds.id)).c_str(), x_values->data(), y_values->data(), y_values->size());
 
@@ -67,4 +50,27 @@ void PlottingWindow::update()
         }
     }
     ImGui::End();
+}
+
+std::tuple<std::vector<double> *, std::vector<double> *> PlottingWindow::get_xy_values(sparq_dataset_t &dataset)
+{
+    std::vector<double> *x_values, *y_values;
+
+    y_values = _data_handler->interpolation ? &dataset.y_values_ip : &dataset.y_values;
+
+    switch (_data_handler->x_axis_select)
+    {
+    default:
+    case 0:
+        x_values = _data_handler->interpolation ? &dataset.samples_ip : &dataset.samples;
+        break;
+    case 1:
+        x_values = _data_handler->interpolation ? &dataset.relative_times_ip : &dataset.relative_times;
+        break;
+    case 2:
+        x_values = _data_handler->interpolation ? &dataset.absolute_times_ip : &dataset.absolute_times;
+        break;
+    }
+
+    return {x_values, y_values};
 }
--- src/PlottingWindow.hpp
+++ src/PlottingWindow.hpp
@@ -10,15 +10,18 @@
 #include <vector>
 #include <string>
 #include <cstdint>
+#include <tuple>
 
 class PlottingWindow
 {
 
 public:
-    PlottingWindow(DataHandler * data_handler);
+    PlottingWindow(DataHandler *data_handler);
     ~PlottingWindow();
 
     void update();
+
+    std::tuple<std::vector<double> *, std::vector<double> *> get_xy_values(sparq_dataset_t &dataset);
 
 private:
     DataHandler *_data_handler;
