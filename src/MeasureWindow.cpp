2e03f9106c792ee4156c4ae31f215cb4506e125b
Fynn Raddatz <fynn.raddatz@gmail.com>
2024-12-10 07:33:03 +0100

    basic marker menu for measure window

--- src/MeasureWindow.cpp
+++ src/MeasureWindow.cpp
@@ -1,7 +1,10 @@
 #include "MeasureWindow.hpp"
 
-MeasureWindow::MeasureWindow()
+MeasureWindow::MeasureWindow(AssetHolder *asset_holder) : _asset_holder(asset_holder)
 {
+    _show_icon_id = _asset_holder->get_handle("icon_visibility_on.png");
+    _hide_icon_id = _asset_holder->get_handle("icon_visibility_off.png");
+    _delete_icon_id = _asset_holder->get_handle("icon_delete.png");
 }
 
 MeasureWindow::~MeasureWindow()
@@ -12,6 +15,77 @@ void MeasureWindow::update()
 {
     if (ImGui::Begin("Measure"))
     {
+        if (ImGui::CollapsingHeader("Markers"))
+        {
+            if (ImGui::Button("Add"))
+            {
+                sparq_marker_t marker;
+                _markers.push_back(marker);
+            }
+
+            ImGui::SameLine();
+
+            if (ImGui::Button("Hide All"))
+            {
+            }
+
+            ImGui::SameLine();
+
+            if (ImGui::Button("Delete All"))
+            {
+            }
+
+            ImGui::Separator();
+
+            measure_markers();
+        }
     }
     ImGui::End();
+}
+
+void MeasureWindow::measure_markers()
+{
+    if (_markers.size() == 0)
+    {
+        ImGui::TextColored(ImVec4(0.6, 0.6, 0.6, 1), "    No Markers");
+    }
+
+    if (ImGui::BeginTable("##MeasureMarkerTable", 5, ImGuiTableFlags_RowBg | ImGuiTableFlags_SizingFixedFit))
+    {
+        std::vector<uint32_t> to_delete;
+        for (size_t i = 0; i < _markers.size(); i++)
+        {
+            std::string i_str = std::to_string(i);
+
+            ImGui::TableNextRow();
+            ImGui::TableSetColumnIndex(0);
+            ImGui::AlignTextToFramePadding();
+            ImGui::Text("M");
+            ImGui::TableSetColumnIndex(1);
+            ImGui::SetNextItemWidth(150);
+            ImGui::Text("Selector");
+            ImGui::TableSetColumnIndex(2);
+            ImGui::ColorEdit4(("##DsColor" + i_str).c_str(), (float *)&_markers[i].color, ImGuiColorEditFlags_NoInputs | ImGuiColorEditFlags_NoLabel);
+            ImGui::TableSetColumnIndex(3);
+
+            ImTextureID hide_icon = _markers[i].hidden ? _show_icon_id : _hide_icon_id;
+            if (ImGui::ImageButton(("HIDE##" + i_str).c_str(), hide_icon, ImVec2(20, 20), ImVec2(0, 0), ImVec2(1, 1), ImVec4(0, 0, 0, 0), ImVec4(0.8, 0.8, 0.8, 1)))
+            {
+                _markers[i].hidden != _markers[i].hidden;
+            }
+
+            ImGui::TableSetColumnIndex(4);
+
+            if (ImGui::ImageButton(("DEL##" + i_str).c_str(), _delete_icon_id, ImVec2(20, 20), ImVec2(0, 0), ImVec2(1, 1), ImVec4(0, 0, 0, 0), ImVec4(0.8, 0.8, 0.8, 1)))
+            {
+                to_delete.push_back(i);
+            }
+        }
+
+        ImGui::EndTable();
+
+        for (auto id : to_delete)
+        {
+        }
+    }
 }
--- src/MeasureWindow.hpp
+++ src/MeasureWindow.hpp
@@ -7,13 +7,20 @@
 
 #include "sparq_types.hpp"
 
+#include "AssetHolder.hpp"
+
 class MeasureWindow
 {
 public:
-    MeasureWindow();
+    MeasureWindow(AssetHolder *asset_holder);
     ~MeasureWindow();
 
     void update();
+    void measure_markers();
 
 private:
+    std::vector<sparq_marker_t> _markers;
+    AssetHolder *_asset_holder;
+
+    ImTextureID _show_icon_id, _hide_icon_id, _delete_icon_id;
 };
