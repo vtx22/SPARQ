78807f98aab1f02fc5478f1f9ac5c7b816e45f6b
Fynn Raddatz <fynn.raddatz@gmail.com>
2026-07-09 20:58:16 +0200

    basic heatmap plot rendering

--- src/windows/plotting/HeatmapPlottingWindow.hpp
+++ src/windows/plotting/HeatmapPlottingWindow.hpp
@@ -21,15 +21,51 @@ namespace spq::ui
         }
 
     private:
-        plotting::timeseries_settings m_settings{};
+        plotting::heatmap_settings m_settings{};
 
     protected:
         void update_plot_contents(data::Datasets& datasets) override
         {
+            auto const pixel_count = static_cast<std::size_t>(m_settings.rows * m_settings.cols);
+            std::vector<float> values(pixel_count);
+            for (std::size_t i{}; auto const& ds : datasets.data())
+            {
+                if (i >= pixel_count)
+                {
+                    break;
+                }
+                values[i++] = ds.y_values.back();
+            }
+
+            auto scale_min{m_settings.scale_min};
+            auto scale_max{m_settings.scale_max};
+
+            if (m_settings.autoscale)
+            {
+                auto [min, max] = std::ranges::minmax(values);
+                scale_min = min;
+                scale_max = max;
+            }
+
+            ImPlot::PlotHeatmap(
+                "Heatmap",
+                values.data(),
+                m_settings.rows,
+                m_settings.cols,
+                scale_min,
+                scale_max,
+                m_settings.show_values ? "%.1f" : "",
+                {0, 0},
+                {
+                    m_settings.normalize_xy ? 1.0 : m_settings.cols,
+                    m_settings.normalize_xy ? 1.0 : m_settings.rows,
+                },
+                0);
         }
 
         [[nodiscard]]
         constexpr ImPlotFlags get_plot_flags() const override
+
         {
             return ImPlotFlags_NoMenus;
         }
